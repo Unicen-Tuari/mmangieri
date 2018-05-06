0cc1df72678e3e35bbc9b5c18e5739c120471755
Mariel Mangieri <marupipi@gmail.com>
2018-05-06 16:19:14 -0300

    agregue tabla de resultado y alert felicitando por la tercera partida

---
+++ juegoJS/js/memoria.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let nroPartida= 0;
+let tableroInicial= false;
+
+function mostrarFelicitacion(nroPartida){
+      if (nroPartida=== 3){
+          alert("Felicitaciones!!!!");
+      }
+}
