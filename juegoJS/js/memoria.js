0e1688a64218d44dd92d2185d0bdf4cbfa81ca07
Mariel Mangieri <marupipi@gmail.com>
2018-05-06 21:46:31 -0300

    correccion de codigo para que invierta las cartas cuando se oprime comenzar por segunda vez

--- juegoJS/js/memoria.js
+++ juegoJS/js/memoria.js
@@ -2,6 +2,11 @@
 
 let nroPartida= 0;
 let tableroInicial= true;
+let cartaDestapada1;
+let cartaDestapada2;
+let cartaDestapada3;
+let cartaDestapada4;
+let cartaDestapada5;
 
 function mostrarFelicitacion(nroPartida){
       if (nroPartida=== 3){
@@ -27,79 +32,99 @@ function generarTablero(){
     carta1= document.getElementById("primerCarta");
     if (probabilidad < 0.5) {
         carta1.src= "images/cartaBlanco.jpg";
+        cartaDestapada1="images/cartaBlanco.jpg";
     }
     else {
         carta1.src= "images/cartaCruz.jpg";
+        cartaDestapada1="images/cartaCruz.jpg";
     }
     probabilidad= Math.random();
     carta2=document.getElementById("segundaCarta");
     if (probabilidad< 0.5) {
         carta2.src= "images/cartaBlanco.jpg";
+        cartaDestapada2="images/cartaBlanco.jpg";
     }
     else {
         carta2.src= "images/cartaCruz.jpg";
+        cartaDestapada2="images/cartaCruz.jpg";
     }
     probabilidad= Math.random();
     carta3=document.getElementById("tercerCarta");
     if (probabilidad< 0.5) {
         carta3.src= "images/cartaBlanco.jpg";
+        cartaDestapada3="images/cartaBlanco.jpg";
     }
     else {
         carta3.src= "images/cartaCruz.jpg";
+        cartaDestapada3="images/cartaCruz.jpg";
     }
     probabilidad= Math.random();
     carta4=document.getElementById("cuartaCarta");
     if (probabilidad< 0.5) {
         carta4.src= "images/cartaBlanco.jpg";
+        cartaDestapada4="images/cartaBlanco.jpg";
     }
     else {
         carta4.src= "images/cartaCruz.jpg";
+        cartaDestapada4="images/cartaCruz.jpg";
     }
     probabilidad= Math.random();
     carta5= document.getElementById("quintaCarta");
     if (probabilidad< 0.5) {
         carta5.src= "images/cartaBlanco.jpg";
+        cartaDestapada5="images/cartaBlanco.jpg";
     }
     else {
         carta5.src= "images/cartaCruz.jpg";
+        cartaDestapada5="images/cartaCruz.jpg";
     }
     tableroInicial= false;
   }
   else {
       carta1= document.getElementById("primerCarta");
-      if(carta1.src ===  "images/cartaBlanco.jpg"){
+      if(cartaDestapada1 ===  "images/cartaBlanco.jpg"){
         carta1.src = "images/cartaCruz.jpg";
+        cartaDestapada1="images/cartaCruz.jpg";
       }
       else {
         carta1.src= "images/cartaBlanco.jpg";
+        cartaDestapada1="images/cartaBlanco.jpg";
       }
       carta2= document.getElementById("segundaCarta");
-      if(carta2.src ===  "images/cartaBlanco.jpg"){
+      if(cartaDestapada2 ===  "images/cartaBlanco.jpg"){
         carta2.src = "images/cartaCruz.jpg";
+        cartaDestapada2="images/cartaCruz.jpg";
       }
       else {
         carta2.src= "images/cartaBlanco.jpg";
+        cartaDestapada2="images/cartaBlanco.jpg";
       }
       carta3= document.getElementById("tercerCarta");
-      if(carta3.src ===  "images/cartaBlanco.jpg"){
+      if(cartaDestapada3 ===  "images/cartaBlanco.jpg"){
         carta3.src = "images/cartaCruz.jpg";
+        cartaDestapada3="images/cartaCruz.jpg";
       }
       else {
         carta3.src= "images/cartaBlanco.jpg";
+        cartaDestapada3="images/cartaBlanco.jpg";
       }
       carta4= document.getElementById("cuartaCarta");
-      if(carta4.src ===  "images/cartaBlanco.jpg"){
+      if(cartaDestapada4 ===  "images/cartaBlanco.jpg"){
         carta4.src = "images/cartaCruz.jpg";
+        cartaDestapada4="images/cartaCruz.jpg";
       }
       else {
         carta4.src= "images/cartaBlanco.jpg";
+        cartaDestapada4="images/cartaBlanco.jpg";
       }
       carta5= document.getElementById("quintaCarta");
-      if(carta5.src ===  "images/cartaBlanco.jpg"){
+      if(cartaDestapada5 ===  "images/cartaBlanco.jpg"){
         carta5.src = "images/cartaCruz.jpg";
+        cartaDestapada5="images/cartaCruz.jpg";
       }
       else {
         carta5.src= "images/cartaBlanco.jpg";
+        cartaDestapada5="images/cartaBlanco.jpg";
       }
   }
 }
