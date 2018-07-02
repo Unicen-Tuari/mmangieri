f546024616d5f5d9b5226c6b37606518bfcc524e
Mariel Mangieri <marupipi@gmail.com>
2018-07-02 15:36:00 -0300

    agregue eliminar libro close #24 y eliminar autor close #23

--- blogLiterario/controller/adminController.php
+++ blogLiterario/controller/adminController.php
@@ -6,109 +6,102 @@ class adminController{
     private $adminView;
     private $blogLiterarioModel;
 
-    function __construct(){
-        $this->adminView= new adminView();
-        $this->blogLiterarioModel= new blogLiterarioModel();
-    }
+	function __construct(){
+		$this->adminView= new adminView();
+		$this->blogLiterarioModel= new blogLiterarioModel();
+	}
 
-    function crearLibro($params = []){
-        $autores= $this->blogLiterarioModel->obtenerAutores();
-        $this->adminView->mostrarCrearLibro($autores);
-    }
+	function crearLibro($params = []){
+		$autores= $this->blogLiterarioModel->obtenerAutores();
+		$this->adminView->mostrarCrearLibro($autores);
+	}
 
+	function guardarLibro($params = []){
+		$libro=['id_autor'=>$_POST['autor'],
+   				'titulo'=> $_POST['titulo'],
+				'genero'=>$_POST['genero'],
+				'sinopsis'=>$_POST['sinopsis']
+					];
+		$this->blogLiterarioModel->insertarLibro($libro);
+		header("Location: ".BASEURL."verLibrosAutorAdmin/".$libro['id_autor']);
+	}
 
-      function guardarLibro($params = []){
-        $libro= ['id_autor'=>$_POST['autor'],
-                  'titulo'=> $_POST['titulo'],
-                  'genero'=>$_POST['genero'],
-                  'sinopsis'=>$_POST['sinopsis']
-                ];
-        $this->blogLiterarioModel->insertarLibro($libro);
-       header("Location: ".BASEURL."verLibrosAutorAdmin/".$libro['id_autor']);
-      }
+	function actualizarLibro($params = []){
+		$libro=['id_libro'=> $params[0],
+				'id_autor'=>$_POST['autor'],
+				'titulo'=>$_POST['titulo'],
+				'genero'=>$_POST['genero'],
+				'sinopsis'=>$_POST['sinopsis']
+				];
+		$this->blogLiterarioModel->editarLibro($libro);
+		header("Location: ".BASEURL."verLibrosAutorAdmin/".$libro['id_autor']);
+	}
 
-      function actualizarLibro($params = []){
-        $libro=['id_libro'=> $params[0],
-                'id_autor'=>$_POST['autor'],
-                'titulo'=>$_POST['titulo'],
-                'genero'=>$_POST['genero'],
-                'sinopsis'=>$_POST['sinopsis']
-              ];
+	function crearAutor($params = []){
+		$this->adminView->mostrarCrearAutor();
+	}
 
-        $this->blogLiterarioModel->editarLibro($libro);
-        header("Location: ".BASEURL."verLibrosAutorAdmin/".$libro['id_autor']);
-      }
+	function guardarAutor($params = []){
+		$autor=['nombre'=>$_POST['nombre'],
+				'biografia'=>$_POST['biografia']
+				];
+		$this->blogLiterarioModel->insertarAutor($autor);
+		header("Location: ".BASEURL."verAutoresAdmin");
+	}
 
-      function crearAutor($params = []){
-          $this->adminView->mostrarCrearAutor();
-      }
+	function actualizarAutor($params = []){
+		$autor=['id_autor'=> $params[0],
+				'nombre'=>$_POST['nombre'],
+				'biografia'=>$_POST['biografia']
+				];
+		$this->blogLiterarioModel->editarAutor($autor);
+		header("Location: ".BASEURL."verAutoresAdmin");
+	}
 
-      function guardarAutor($params = []){
-        $autor=['nombre'=>$_POST['nombre'],
-                'biografia'=>$_POST['biografia']
-              ];
-        $this->blogLiterarioModel->insertarAutor($autor);
-        header("Location: ".BASEURL."verAutoresAdmin");
-      }
+	function borrarLibro($params = []){
+		$libro= $this->blogLiterarioModel->obtenerLibro($params[0]);
+		$autor=$this->blogLiterarioModel->obtenerAutor($libro['id_autor']);
+		$this->blogLiterarioModel->borrarLibro($params[0]);
+		header("Location: ".BASEURL."verLibrosAutorAdmin/".$libro['id_autor']);
+	}
 
-      function actualizarAutor($params = []){
-        $autor=['id_autor'=> $params[0],
-                'nombre'=>$_POST['nombre'],
-                'biografia'=>$_POST['biografia']
-              ];
+	function borrarAutor($params = []){
+		$this->blogLiterarioModel->borrarAutor($params[0]);
+		header("Location: ".BASEURL."verAutoresAdmin");
+	}
 
-        $this->blogLiterarioModel->editarAutor($autor);
-        header("Location: ".BASEURL."verAutoresAdmin");
-      }
+	function editarAutor($params = []){
+		$autor= $this->blogLiterarioModel->obtenerAutor($params[0]);
+		$this->adminView->mostrarEditarAutor($autor);
+	}
 
-      function borrarLibro($params = []){
-          $libro= $this->blogLiterarioModel->obtenerLibro($params[0]);
-          $autor=$this->blogLiterarioModel->obtenerAutor($libro['id_autor']);
-          $this->blogLiterarioModel->borrarLibro($params[0]);
-          header("Location: ".BASEURL."verLibrosAutorAdmin/".$libro['id_autor']);
+	function editarLibro($params = []){
+		$autores= $this->blogLiterarioModel->obtenerAutores();
+		$libro= $this->blogLiterarioModel->obtenerLibro($params[0]);
+		$this->adminView->mostrarEditarLibro($libro, $autores);
+	}
 
+	function mostrarAutores($params=[]){
+		$autores= $this->blogLiterarioModel->obtenerAutores();
+		$this->adminView->mostrarAutores($autores);
+	}
 
-      }
+	function mostrarLibro($params= []){
+		$libro=$this->blogLiterarioModel->obtenerLibro($params[0]);
+		$autor= $this->blogLiterarioModel->obtenerAutor($libro['id_autor']);
+		$this->adminView->mostrarLibro($libro, $autor);
+	}
 
-      function borrarAutor($params = []){
-             $this->blogLiterarioModel->borrarAutor($params[0]);
-            header("Location: ".BASEURL."verAutoresAdmin");
-          }
+	function mostrarLibrosAutor($params= []){
+		$librosAutor= $this->blogLiterarioModel->obtenerLibrosAutor($params[0]);
+		$autor= $this->blogLiterarioModel->obtenerAutor($params[0]);
+		$this->adminView->mostrarLibrosAutor($librosAutor, $autor);
+	}
 
-      function editarAutor($params = []){
-            $autor= $this->blogLiterarioModel->obtenerAutor($params[0]);
-            $this->adminView->mostrarEditarAutor($autor);
-      }
-
-      function editarLibro($params = []){
-        $autores= $this->blogLiterarioModel->obtenerAutores();
-        $libro= $this->blogLiterarioModel->obtenerLibro($params[0]);
-            $this->adminView->mostrarEditarLibro($libro, $autores);
-
-      }
-
-          function mostrarAutores($params=[]){
-            $autores= $this->blogLiterarioModel->obtenerAutores();
-            $this->adminView->mostrarAutores($autores);
-          }
-
-          function mostrarLibro($params= []){
-              $libro=$this->blogLiterarioModel->obtenerLibro($params[0]);
-              $autor= $this->blogLiterarioModel->obtenerAutor($libro['id_autor']);
-              $this->adminView->mostrarLibro($libro, $autor);
-          }
-
-          function mostrarLibrosAutor($params= []){
-              $librosAutor= $this->blogLiterarioModel->obtenerLibrosAutor($params[0]);
-              $autor= $this->blogLiterarioModel->obtenerAutor($params[0]);
-              $this->adminView->mostrarLibrosAutor($librosAutor, $autor);
-          }
-
-
-          function listarLibros($params= []){
-              $libros= $this->blogLiterarioModel->listarLibros();
-              $autores= $this->blogLiterarioModel->obtenerAutores();
-              $this->blogLiterarioView->listarLibros($libros, $autores);
-          }
+	function listarLibros($params= []){
+		$libros= $this->blogLiterarioModel->listarLibros();
+		$autores= $this->blogLiterarioModel->obtenerAutores();
+		$this->blogLiterarioView->listarLibros($libros, $autores);
+	}
 }
 ?>
