b8426844772b1ed2e6516fb3391a4af78d9cbfe7
Mariel Mangieri <marupipi@gmail.com>
2018-07-03 16:45:49 -0300

    agregue la pagina de helpers

---
+++ blogLiterario/helpers/PageHelpers.php
@@ -0,0 +1,38 @@
+<?php
+
+class PageHelpers
+{
+  public static function homePage()
+  {
+    header("Location: ".BASEURL."listarLibros");
+    die();
+  }
+
+  public static function loginPage()
+  {
+    header("Location: ".BASEURL."login");
+    die();
+  }
+
+  public static function logoutPage()
+  {
+    header("Location: ".BASEURL."logout");
+    die();
+  }
+
+  public static function librosAutorPage($libro)
+  {
+    header("Location: ".BASEURL."verLibrosAutor/".$libro['id_autor']);
+    die();
+  }
+
+  public static function AutoresPage()
+  {
+    header("Location: ".BASEURL."verAutores");
+    die();
+  }
+
+}
+
+
+ ?>
