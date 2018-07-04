717102fb4fc65f3d4da4a4a4ac5cacf46f099e0e
Mariel Mangieri <marupipi@gmail.com>
2018-07-04 00:52:04 -0300

    agregue el config close #9

--- blogLiterario/config.php
+++ blogLiterario/config.php
@@ -1,18 +1,36 @@
 <?php
 $acciones = [
-  // "" => "", //pagina principal
-  // "login" => "LoginController#validarLogin",
-  // "logout" => "LoginController#logout",
-//  "ver"
-  "listarLibros"=>"blogLiterarioController*listarLibros",
-  "verAutores"=> "blogLiterarioController*mostrarAutores",
-  "verLibrosAutor"=>"blogLiterarioController*mostrarLibrosAutor",
-  "verLibro"=>"blogLiterarioController*mostrarLibro",
-  "crearLibro"=>"blogLiterarioController*crearLibro",
-  "guardarLibro"=>"blogLiterarioController*guardarLibro",
-  "crearAutor"=>"blogLiterarioController*crearAutor",
-  "guardarAutor"=>"blogLiterarioController*guardarAutor",
-  "borrarLibro"=>"blogLiterarioController*borrarLibro",
-  "borrarAutor"=>"blogLiterarioController*borrarAutor"
+   "" => "libroController*listarLibros", //pagina principal
+
+   "login" => "loginController*Login",
+   "validarLogin"=>"loginController*validarLogin",
+   "logout" => "loginController*logout",
+   "registro"=> "loginController*registrarse",
+   "ingresarUsuario"=> "usuariosController*ingresarUsuario",
+   "listarUsuarios"=>"usuariosController*listarUsuarios",
+   "editarPermisos"=>"usuariosController*editarPermisos",
+   "borrarUsuario"=>"usuariosController*borrarUsuario",
+
+   "listarLibros"=>"libroController*listarLibros",
+
+  "verAutores"=> "autorController*mostrarAutores",
+
+  "verLibrosAutor"=>"libroController*mostrarLibrosAutor",
+
+  "verLibro"=>"libroController*mostrarLibro",
+
+  "crearLibro"=>"libroController*crearLibro",
+  "editarLibro"=>"libroController*editarLibro",
+  "actualizarLibro"=>"libroController*actualizarLibro",
+  "guardarLibro"=>"libroController*guardarLibro",
+  "borrarLibro"=>"libroController*borrarLibro",
+
+  "crearAutor"=>"autorController*crearAutor",
+  "editarAutor"=>"autorController*editarAutor",
+  "actualizarAutor"=>"autorController*actualizarAutor",
+  "guardarAutor"=>"autorController*guardarAutor",
+  "borrarAutor"=>"autorController*borrarAutor",
+
+  "mostrarDetalleAutor"=>"autorController*mostrarDetalleAutor"
 ];
 ?>
